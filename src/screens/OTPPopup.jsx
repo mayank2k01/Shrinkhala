6e445972b2a6178805a870ab71ac967374f667c5
sudarshan14501 <sudarshan@natoe.ai>
2025-11-28 16:51:10 +0530

    Latest App changes

--- src/screens/OTPPopup.jsx
+++ src/screens/OTPPopup.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Modal, StyleSheet, TouchableOpacity } from 'react-native';
 
 const OtpPopup = ({ userName, otp, onClose }) => {
+
     return (
         <Modal
             transparent={true}
